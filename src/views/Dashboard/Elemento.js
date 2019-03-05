c0e99dc0df4b8826c9d8617dfc8738e1daa3d28a
felipesilvadv <lfsilva1@uc.cl>
2019-03-05 15:51:13 -0300

    View rescale

--- src/views/Dashboard/Elemento.js
+++ src/views/Dashboard/Elemento.js
@@ -7,7 +7,7 @@ class Elemento extends Component {
     return(
       <ListGroupItem key={this.props.dato.id} >
         <div className="row">
-          <div className="col-1">{this.props.dato.orden}</div>
+          <div className="col">{this.props.dato.orden}</div>
           <div className="col">{this.props.dato.cliente}</div>
           <div className="col-3">
             {moment(this.props.dato.updatedAt).format("HH:mm")}
